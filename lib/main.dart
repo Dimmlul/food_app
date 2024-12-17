d16510f3ad664db648bdb82a28ee23cbcc400bff
Dimm <dimaspradiptam@gmail.com>
2024-12-17 18:13:46 +0700

    TabIndex GetXController and Obx

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_app/constants/constants.dart';
@@ -10,7 +11,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/constants/constants.dart';
+import 'package:food_app/controllers/tab_index_controller.dart';
+import 'package:get/get.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final controller = Get.put(TabIndexController());
+    return Obx(() => Scaffold(
       body: Stack(
         children: [
           Container(
@@ -21,6 +25,15 @@ class MainScreen extends StatelessWidget {
                 canvasColor: (kPrimary),
               ),
               child: BottomNavigationBar(
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                unselectedIconTheme: const IconThemeData(color: Colors.black),
+                selectedIconTheme: const IconThemeData(color: kSecondary),
+                onTap: (value){
+                  controller.setTabIndex = value;
+                },
+
+                currentIndex: controller.tabIndex,
                 items: const [
                   BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
                   BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
@@ -32,6 +45,7 @@ class MainScreen extends StatelessWidget {
           ),
         ],
       ),
+    )
     );
   }
 }
